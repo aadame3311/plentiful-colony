bfd70f2269884bbbe84a8126834b9379a42b5e4c
root <root@DESKTOP-2A2R90E.localdomain>
2024-03-02 22:27:56 -0600

    test: adds csharpier as default formatter

--- components/Health.cs
+++ components/Health.cs
@@ -3,51 +3,54 @@ using Godot;
 [GlobalClass]
 public partial class Health : Node
 {
-  [Export]
-  public double health = 100;
-  public Interactable parent;
+    [Export]
+    public double health = 100;
+    public Interactable parent;
 
-  [Signal]
-  public delegate void OnHealthHitEventHandler();
+    [Signal]
+    public delegate void OnHealthHitEventHandler();
 
-  [Signal]
-  public delegate void OnHealthDepleteEventHandler();
+    [Signal]
+    public delegate void OnHealthDepleteEventHandler();
 
-  public override void _Ready()
-  {
-    base._Ready();
-    parent = GetParent<Interactable>();
-    parent.OnHitDetected += (Node2D node) => { HealthHitHandler(node); };
-  }
-
-  public void HealthHitHandler(Node2D node)
-  {
-    if (node is Villager)
+    public override void _Ready()
     {
-      Villager villager = node as Villager;
+        base._Ready();
+        parent = GetParent<Interactable>();
+        parent.OnHitDetected += (Node2D node) =>
+        {
+            HealthHitHandler(node);
+        };
+    }
 
-      // safety check to ensure interactor is the same as the tree the villager is hitting
-      if (villager.treeToChop == parent)
-      {
-        health -= villager.creatureData.creatureController.treeDamage;
+    public void HealthHitHandler(Node2D node)
+    {
+        if (node is Villager)
+        {
+            Villager villager = node as Villager;
 
+            // safety check to ensure interactor is the same as the tree the villager is hitting
+            if (villager.treeToChop == parent)
+            {
+                health -= villager.creatureData.creatureController.treeDamage;
+
+                if (health <= 0)
+                {
+                    HealthDepleteHandler();
+                }
+            }
+        }
+    }
+
+    public void HealthDepleteHandler()
+    {
         if (health <= 0)
         {
-          HealthDepleteHandler();
+            if (parent is Tree)
+            {
+                Tree treeNode = parent as Tree;
+                treeNode.RemoveTree();
+            }
         }
-      }
     }
-  }
-
-  public void HealthDepleteHandler()
-  {
-    if (health <= 0)
-    {
-      if (parent is Tree)
-      {
-        Tree treeNode = parent as Tree;
-        treeNode.RemoveTree();
-      }
-    }
-  }
 }
